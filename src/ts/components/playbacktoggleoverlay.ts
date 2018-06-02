5b71bafd51f8f3424dc5ab530fe135a16e360c01
louis_Chiang <louis@giloo.ist>
2018-06-02 17:10:41 +0800

    Add Beta hugePlayContainer

--- src/ts/components/playbacktoggleoverlay.ts
+++ src/ts/components/playbacktoggleoverlay.ts
@@ -1,5 +1,6 @@
 import {Container, ContainerConfig} from './container';
 import {HugePlaybackToggleButton} from './hugeplaybacktogglebutton';
+import { PlaybackTimeLabel, PlaybackTimeLabelMode } from './playbacktimelabel';
 
 /**
  * Overlays the player and displays error messages.
@@ -15,7 +16,13 @@ export class PlaybackToggleOverlay extends Container<ContainerConfig> {
 
     this.config = this.mergeConfig(config, {
       cssClass: 'ui-playbacktoggle-overlay',
-      components: [this.playbackToggleButton],
+      components: [
+        this.playbackToggleButton,
+        // reverse
+        // fast-forward
+        // time remaining
+        new PlaybackTimeLabel({ timeLabelMode: PlaybackTimeLabelMode.CurrentAndTotalTime,cssClass: 'ui-hugePlaybackTimeLabel' })
+      ],
     }, this.config);
   }
 }
